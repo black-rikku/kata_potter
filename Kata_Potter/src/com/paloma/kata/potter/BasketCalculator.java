d97c733b89dc995a09cbe1cd92d0dcc5cf6b1bf4
black-rikku <pmp_paloma@hotmail.com>
2016-05-07 19:39:52 +0200

    second version

--- Kata_Potter/src/com/paloma/kata/potter/BasketCalculator.java
+++ Kata_Potter/src/com/paloma/kata/potter/BasketCalculator.java
@@ -1,5 +1,9 @@
 package com.paloma.kata.potter;
 
-public class BasketCalculator {
+import java.util.List;
 
+public class BasketCalculator {
+	public int calculatePrice(List<String> books) {
+        return -1;
+	}
 }
