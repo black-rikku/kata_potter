8907009492662537900184d885cb1aafba72159e
black-rikku <pmp_paloma@hotmail.com>
2016-05-08 11:49:36 +0200

    Some changes in the test

--- Kata_Potter/src/com/paloma/kata/potter/BasketCalculatorTest.java
+++ Kata_Potter/src/com/paloma/kata/potter/BasketCalculatorTest.java
@@ -10,61 +10,55 @@ public class BasketCalculatorTest {
 	private static final List<String> EMPTY_BASKET = new ArrayList<>();
 	private static final String FIRST_BOOK = "The Sorcerer's Stone";
 	private static final String SECOND_BOOK = "The Chamber of Secrets";
+	private static final String THIRD_BOOK = "The Prisoner of Azkaban";
+	private static final String FOURTH_BOOK = "The Goblet of Fire";
+	private static final String FIFTH_BOOK = "The Order of the Phoenix";
 	private static final int DELTA = 0;
 
-	  @Test
-	  public void shouldReturnZeroPriceForAnEmptyBasket() {
-	    BasketCalculator basketCalculator = new BasketCalculator();
-
-	    int amount = basketCalculator.calculatePrice(EMPTY_BASKET);
-
-	    assertEquals(0, amount);
-	  }
-	  
-	  @Test
-	  public void shouldReturnPriceWithoutDiscountForABasketWithOneBook() {
-		  BasketCalculator basketCalculator = new BasketCalculator();
-		  List <String> basket = givenBasketWithOneBook();
-		  int amount = basketCalculator.calculatePrice(basket);
-
-		  assertEquals(8, amount);
-	  }
-	  
-	  private List<String> givenBasketWithOneBook() {
-		    List<String> basket = new ArrayList<>();
-		    basket.add(FIRST_BOOK);
-		    return basket;
-		  }
-	  
-	  @Test
-	  public void shouldReturnPriceWithoutDiscountForABasketWithTwoEqualBooks() {
-		  BasketCalculator basketCalculator = new BasketCalculator();
-		  List <String> basket = givenBasketWithTwoEqualBooks();
-		  int amount = basketCalculator.calculatePrice(basket);
-
-		  assertEquals(8*2, amount);
-	  }
-	  
-	  private List<String> givenBasketWithTwoEqualBooks() {
-		    List<String> basket = new ArrayList<>();
-		    basket.add(FIRST_BOOK);
-		    basket.add(FIRST_BOOK);
-		    return basket;
-		  }
-	  
 	@Test
-	  public void shouldReturnPriceWithDiscountForABasketWithTwoDiferentBooks() {
-		  BasketCalculator basketCalculator = new BasketCalculator();
-		  List <String> basket = givenBasketWithTwoDiferentBooks();
-		  float amount = basketCalculator.calculatePriceWithDiscount(basket);
+	public void shouldReturnZeroPriceForAnEmptyBasket() {
+		BasketCalculator basketCalculator = new BasketCalculator();
 
-		  assertEquals(2*8*0.95f, amount, DELTA);
-	  }
-	  
-	  private List<String> givenBasketWithTwoDiferentBooks() {
-		    List<String> basket = new ArrayList<>();
-		    basket.add(FIRST_BOOK);
-		    basket.add(SECOND_BOOK);
-		    return basket;
-		  }
+		int amount = basketCalculator.calculatePrice(EMPTY_BASKET);
+
+		assertEquals(0, amount);
+	}
+
+	@Test
+	public void shouldReturnPriceWithoutDiscountForABasketWithOneBook() {
+		BasketCalculator basketCalculator = new BasketCalculator();
+		List<String> basket = givenBasketWithBooks(FIRST_BOOK);
+		int amount = basketCalculator.calculatePrice(basket);
+
+		assertEquals(8, amount);
+	}
+
+	@Test
+	public void shouldReturnPriceWithoutDiscountForABasketWithTwoEqualBooks() {
+		BasketCalculator basketCalculator = new BasketCalculator();
+		List<String> basket = givenBasketWithBooks(SECOND_BOOK, SECOND_BOOK);
+		int amount = basketCalculator.calculatePrice(basket);
+
+		assertEquals(8 * 2, amount);
+	}
+
+	@Test
+	public void shouldReturnPriceWithDiscountForABasketWithTwoDiferentBooks() {
+		BasketCalculator basketCalculator = new BasketCalculator();
+		List<String> basket = givenBasketWithBooks(FOURTH_BOOK, FIFTH_BOOK);
+		float amount = basketCalculator.calculatePriceWithDiscount(basket);
+
+		assertEquals(2 * 8 * 0.95f, amount, DELTA);
+	}
+
+	private List<String> givenBasketWithBooks(String... books) {
+		List<String> basket = new ArrayList<>();
+		if (books != null) {
+			for (String book : books) {
+				basket.add(book);
+			}
+		}
+		return basket;
+	}
+
 }
